eb795dc620a7f2d984d9293eaefb230b87278808
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-29 02:57:40 -0700

    added shift method

--- src/index.js
+++ src/index.js
@@ -38,4 +38,23 @@ class DoublyLinkedList {
     this.length--;
     return this;
   }
+  shift() {
+    if (this.length === 0) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
 }
+
+var list = new DoublyLinkedList();
+list.push("Harry");
+list.push("Ron");
+list.push("Hermione");
